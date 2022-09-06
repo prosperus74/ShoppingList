59769095ef2bcdb247686a166ef1666a538fde9d
prosperus74 <77073082+prosperus74@users.noreply.github.com>
2022-09-06 15:22:08 +0300

    DataBase

---
+++ app/src/main/java/com/krock/shoppinglist/data/ShopItemDbModel.kt
@@ -0,0 +1,15 @@
+package com.krock.shoppinglist.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.krock.shoppinglist.domain.ShopItem
+
+
+@Entity (tableName = "shop_items")
+data class ShopItemDbModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val count: Int,
+    var enabled: Boolean,
+)
